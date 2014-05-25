6f03f4a487e45e6b9a2eb3ad5c6bec7f8039cbbe
armsha <armsha@users.noreply.github.com>
2014-05-26 01:10:42 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Code for computing and hashing inverses of square matrices. 
+## Function explanations:
+##      - makeCacheMatrix
+##              - Input: A square matrix with numeric elements
+##              - Output: A list of functions for storing and
+##              -         retrieving the input matrix and the 
+##                        corresponding inverse
+##      - cacheSolve
+##              - Input: A square matrix with numeric elements,
+##                       ev. other input is ignored
+##              - Output: The inverse of the input matrix
+##
+## Written as part of the Coursera course "R Programming", spring 2014
+## v.1.1, written by A.Shamsgovara, last update 26 may 2014, 1.09 AM CEST
 
-## Write a short comment describing this function
+## Function for getting functions for storing cache keys and
+## values, where both keys and values are matrices
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) { ## Setting the key
+                x <<- y
+                m <<- NULL
+        }     
+        get <- function() x ## Retrieving the key
+        setinverse <- function(solve) m <<- solve ## Setting the stored value
+        getinverse <- function() m ## Retrieving the stored value
+        list(set = set, get = get,
+                 setinverse = setinverse,
+                 getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Function for computing the inverse of an input 
+## square nonsingular matrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse() ## Try to get the inverse from cache
+        if(!is.null(m)) { ## Inverse already in cache, return it
+                return(m)
+        }
+        data <- x$get() ## Get the matrix
+        m <- solve(data, ...) ## Compute inverse
+        x$setinverse(m) ## Store in cache memory
+        m # Return inverse
 }
